543a9d1be2af91378591555e294e009706f13f21
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-18 14:29:33 +0900

    Add first three sidebar icons

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div className="">
-      <Head>
-        <title>Spotify 2.0</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>This is a DOPE spotify 2</h1>
+      <main>
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Center */}
+      </main>
     </div>
   );
 }
