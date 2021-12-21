674e21107356e77aa9e4586fc499ee45493df142
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-21 14:46:46 +0900

    Add center panel

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,7 +13,10 @@ const Sidebar = () => {
 
   console.log("session", session);
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div
+      className="text-gray-500 p-5 text-sm border-r border-gray-900
+    overflow-y-scroll h-screen"
+    >
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
+import Center from "./Center";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
+      <main className="flex">
         {/* Sidebar */}
         <Sidebar />
+        <Center />
         {/* Center */}
       </main>
     </div>
