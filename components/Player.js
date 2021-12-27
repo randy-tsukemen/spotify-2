e4294bc8cd99c663bc71a142bd0e2127708fff44
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-27 23:13:46 +0900

    click to increase or lower the volume

--- components/Player.js
+++ components/Player.js
@@ -93,15 +93,22 @@ const Player = () => {
 
       {/* Right */}
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeDownIcon className="button" />
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
         <input
           className="w-14 md:w-28"
           type="range"
           min="0"
           max="100"
           value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
         />
-        <VolumeUpIcon className="button" />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
